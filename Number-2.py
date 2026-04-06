53e302e9147ef9d51b9399ae3497a40639ea8499
Michellek207 <kharusimichelle207@gmail.com>
2026-04-06 16:56:39 +0300

    Update Number-2.py

--- Number-2.py
+++ Number-2.py
@@ -1,7 +1,7 @@
 def convert(fahrenheit):
   celsius= (fahrenheit - 32) * 5/9
   return celsius
-fahrenheit_input= float(input("Enter the temperature i fahrenheit:"))
+fahrenheit_input= float(input("Enter the temperature in Fahrenheit:"))
 celsius_value= convert(fahrenheit_input)
 print(f"{fahrenheit_input} Fahrenheit is equal to {celsius_value:.2f} Celsius")
 if celsius_value > 20:
